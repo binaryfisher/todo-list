b47012de08613cd0285fa26bfa4f4fbc2a5081bb
Wilson Yu <wilsonbinaryfisher@gmail.com>
2023-03-21 16:52:20 -0400

    half

--- script.js
+++ script.js
@@ -5,11 +5,64 @@ const addPrjFormAddBtn = document.querySelector("#pjt-form-add");
 const addPjtValidationBlock = document.querySelector("#add-pjt-vd");
 const addPjtInput = document.getElementById("pjt-name");
 const projectsOverview = document.querySelector(".projects-overview")
-
-
+const displayHeaderName = document.querySelector(".display-header-name")
+const cardsContainer = document.querySelector(".cards-container")
 let addPjtInputValid = false;
+const projects = [];
+let currentProject = null;
+
+class ToDoItem{
+    constructor(title, description, dueDate){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+}
+
+class Project{
+      
+    constructor(name){
+      this.name = name;
+      this.toDoItems = [];
+    }
+    addToDoItem(item){
+        this.toDoItems.push(item)
+    };
+
+    removeToDoItem(item){
+       const index = this.toDoItems.indexOf(item);
+       this.toDoItems.splice(index, 1);
+    }
+    getProjectName(){
+        return this.name;
+    }
+
+};
+
+const removeAllChild = (parent) =>{
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const updateCardDisplay = () =>{
+    removeAllChild(cardsContainer);
+    projects.forEach((project) =>{
+        let cardDiv = document.createElement("div");
+        let cardHeader = document.createElement("div")
+        cardDiv.className="card";
+        cardHeader.className="card-header";
+        cardHeader.innerHTML = project.getProjectName();
+        cardDiv.appendChild(cardHeader);
+        
+        cardsContainer.appendChild(cardDiv);
+    })
+}
 
 addPjtBtn.addEventListener("click", () => {
+    addPjtValidationBlock.innerHTML = "";
+    addPjtInputValid = false;
+    addPjtValidationBlock.classList.remove("valid");
     addPjtFormContainer.style.display = "flex";
 });
 
@@ -18,15 +71,36 @@ addPrjFormCxlBtn.addEventListener("click", () =>{
     addPjtFormContainer.style.display = "none";
 } );
 
+const handlePjtClick = (event) =>{
+    const pjtDivs = document.querySelectorAll(".project-name");
+    pjtDivs.forEach((element) =>{
+        element.classList.remove("selected");
+    })
+    event.target.classList.add("selected");
+    projects.forEach((project) =>{
+        if(project.getProjectName() == event.target.innerHTML){
+            currentProject = project;
+            displayHeaderName.innerHTML = project.getProjectName();
+        }
+    })
+
+   
+}
+
 addPrjFormAddBtn.addEventListener("click", () =>{
     
    
     if(addPjtInputValid){
+        let pjtName = addPjtInput.value;
+        let project = new Project(pjtName);
+        projects.push(project);
         let pjtDiv = document.createElement("div");
         pjtDiv.className = "project-name";
-        pjtDiv.innerHTML = addPjtInput.value;
+        pjtDiv.innerHTML = project.getProjectName();
+        pjtDiv.addEventListener("click", handlePjtClick)
         projectsOverview.appendChild(pjtDiv);
         addPjtFormContainer.style.display = "none";
+        updateCardDisplay();
     }else{
         addPjtValidationBlock.innerHTML = "Please enter a valid name!"
     }
@@ -35,7 +109,7 @@ addPrjFormAddBtn.addEventListener("click", () =>{
 });
 
 addPjtInput.addEventListener("input", (event)=>{
-
+    
     let inputValue = addPjtInput.value;
     if(!inputValue){
         addPjtInputValid = false;
@@ -45,16 +119,27 @@ addPjtInput.addEventListener("input", (event)=>{
         addPjtValidationBlock.classList.remove("valid");
         addPjtValidationBlock.innerHTML = "Too short";
     }else if(inputValue.length < 20){
-        addPjtInputValid = true;
-        addPjtValidationBlock.classList.add("valid")
-        addPjtValidationBlock.innerHTML = "Valid"
+        const pjtNames = [];
+        projects.forEach((project) =>{
+            pjtNames.push(project.getProjectName())
+        });
+        if(pjtNames.includes(inputValue.trim())){
+            addPjtInputValid = false;
+            addPjtValidationBlock.classList.remove("valid")
+            addPjtValidationBlock.innerHTML = "Project already exist!"
+        }else{
+            addPjtInputValid = true;
+            addPjtValidationBlock.classList.add("valid")
+            addPjtValidationBlock.innerHTML = "Valid"
+        }
+       
     }else{
         addPjtInputValid = false;
         addPjtValidationBlock.classList.remove("valid")
         addPjtValidationBlock.innerHTML = "Too long"
     }
    
-})
+});
 
 
 
